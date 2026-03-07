a6f7a2275b2a81c989d00970fddf0f2f224091e1
LAB13_USER ON WS36 <L13Y09W36@LabHCI.net>
2026-03-07 10:37:51 +0800

    feat(backend): initialize Spring Boot project with Maven, MySQL config, and dependencies

---
+++ Backend/campusgear/campusgear/src/main/java/edu/cit/abadinas/campusgear/CampusgearApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.abadinas.campusgear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusgearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusgearApplication.class, args);
+	}
+
+}
---
+++ Backend/campusgear/campusgear/src/test/java/edu/cit/abadinas/campusgear/CampusgearApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.abadinas.campusgear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusgearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
